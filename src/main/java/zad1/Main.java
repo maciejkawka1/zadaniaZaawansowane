9a14d1e9b1bc51d27f72bd083617b21833c54d3a
maciejkawka1 <kawkamaciej.mk@gmail.com>
2024-02-27 18:10:28 +0100

    poprawa sortowania i dodanie opcji z sort

--- src/main/java/zad1/Main.java
+++ src/main/java/zad1/Main.java
@@ -7,13 +7,14 @@ public class Main {
     public static void main(String[] args) {
         List<String> listaStringow = new ArrayList<>();
         listaStringow.add("zamek");
+        listaStringow.add("auto");
         listaStringow.add("kot");
         listaStringow.add("pies");
         listaStringow.add("wrota");
         listaStringow.add("listewka");
         listaStringow.add("drzwi");
 
-        listaStringow.forEach(System.out::println);
+
 
         System.out.println("====Posortowana z Collections====");
         List<String> sorted = sortListCollections(listaStringow);
@@ -24,17 +25,34 @@ public class Main {
         sorted2.forEach(System.out::println);
 
 
+        System.out.println("====Posortowana z metodą sort====");
+        List<String> sorted3 = sortListSortMethod(listaStringow);
+        sorted3.forEach(System.out::println);
+
+        System.out.println("===Oryginał===");
+        listaStringow.forEach(System.out::println);
+
     }
 
     public static List<String> sortListCollections(List<String> list){
         List<String> toSort = new ArrayList<>(list);
         Collections.sort(toSort);
+        Collections.reverse(toSort);
         return toSort;
     }
 
     public static List<String> sortListStream(List<String> list){
         List<String> toSort = new ArrayList<>(list);
 
-        return toSort.stream().sorted().collect(Collectors.toList());
+        return toSort.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
     }
+
+    public static List<String> sortListSortMethod(List<String> list){
+        List<String> toSort = new ArrayList<>(list);
+
+        toSort.sort(Comparator.reverseOrder());
+        return toSort;
+    }
+
+
 }
